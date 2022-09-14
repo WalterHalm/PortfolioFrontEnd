a52d1e010b69cf27b331724979476a43f6b8095b
WalterHalm <110131597+WalterHalm@users.noreply.github.com>
2022-09-13 22:02:31 -0300

    Proyecto Integrador
    Aun no esta terminado, hecho hasta el dia de la fecha la maqueta

---
+++ script.js
@@ -0,0 +1,33 @@
+//seleccionar aplica estilo a la opcion seleccionada y quita previamente la seleccionada
+
+function seleccionar(navmedio){
+    var opciones = document.querySelectorAll('#navmedio a')
+    opciones [0].className ="";
+    opciones [1].className ="";
+    opciones [2].className ="";
+    opciones [3].className ="";
+    opciones [4].className ="";
+    opciones [5].className ="";                
+    navmedio.className = "seleccionado";
+
+    
+    
+
+//hacemos desaparece menu responsive
+var x = document.getElementById("nav");
+x.className="";
+}
+
+
+
+
+
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+
+}
